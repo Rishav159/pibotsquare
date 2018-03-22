66e3b92e6a2a028a77e4b4f6cb169ca3314928b0
Rishav Agarwal <rishav.159@gmail.com>
2018-03-23 00:52:56 +0530

    Integrates socket to emit movement messages

--- index.py
+++ index.py
@@ -2,9 +2,10 @@ import numpy as np
 import os.path
 from flask import Flask, Response, send_from_directory, render_template
 from camera import VideoCamera
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__,static_url_path='/static')
-
+socketio = SocketIO(app)
 @app.route('/')
 def root_dir():
     return render_template('index.html')
@@ -20,5 +21,9 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@socketio.on('move')
+def handle_message(move):
+    print(move)
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
-function buttonPressed(btn){
+var socket;
+function buttonPressed(btn,key){
   $('#'+btn).addClass("active");
+  if(key){
+    socket.emit('move',key);
+  }
 }
 
 function buttonRelease(btn){
@@ -21,7 +25,6 @@ function captureImage(){
   let fileName = date.toLocaleDateString().replace(new RegExp('/',"g"),'-')+'_'+date.toLocaleTimeString().replace(new RegExp(':',"g"),'-').replace(new RegExp(' ',"g"),'-')+'.jpg';
   let ele = $('#currentImage');
   let b64 = getBase64Image(document.getElementById('currentImage'));
-  console.log(b64);
   let link = document.createElement('a')
   link.href = b64;
   link.download = fileName;
@@ -29,20 +32,23 @@ function captureImage(){
   $('#captured-image').append(link);
   link.click();
 }
-
+function initSocketIO() {
+       socket = io.connect('//' + document.domain + ':' + location.port);
+}
 $(document).ready(function(){
+  initSocketIO();
   //KeyPress Event
   $(document).keydown(function(e){
     let key = e.originalEvent.key;
     switch (key) {
       case 'ArrowUp':
-      case 'w' : buttonPressed('forward-button'); break;
+      case 'w' : buttonPressed('forward-button','f'); break;
       case 'ArrowDown':
-      case 's' : buttonPressed('back-button'); break;
+      case 's' : buttonPressed('back-button','b'); break;
       case 'ArrowLeft':
-      case 'a' : buttonPressed('left-button'); break;
+      case 'a' : buttonPressed('left-button','l'); break;
       case 'ArrowRight':
-      case 'd' : buttonPressed('right-button'); break;
+      case 'd' : buttonPressed('right-button','r'); break;
       case ' ': buttonPressed('capture-button'); break;
       default:
     }
@@ -66,12 +72,12 @@ $(document).ready(function(){
   //Scroll up events
   $(document).bind('DOMMouseScroll mousewheel',function(e){
     if(e.originalEvent.wheelDelta/120 > 0 || e.originalEvent.detail < 0){
-      buttonPressed('camera-up');
+      buttonPressed('camera-up','u');
       setTimeout(function(){
         buttonRelease('camera-up');
       },250);
     }else{
-      buttonPressed('camera-down');
+      buttonPressed('camera-down','d');
       setTimeout(function(){
         buttonRelease('camera-down');
       },250);
